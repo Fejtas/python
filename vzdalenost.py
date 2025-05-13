d91e1be46fbe3e9a09e0ecd5f60ae7d37a2f244b
Jíří Fejtek <fejtas@post.cz>
2025-05-13 08:21:43 +0200

    Create vzdalenost.py

---
+++ vzdalenost.py
@@ -0,0 +1,11 @@
+import math
+
+# Souřadnice bodů
+A = (5, 1)
+B = (-2, 18)
+
+# Výpočet vzdálenosti
+vzdalenost = math.sqrt((B[0] - A[0])**2 + (B[1] - A[1])**2)
+
+# Výpis výsledku
+print("Vzdálenost mezi body A a B je:", vzdalenost)  
